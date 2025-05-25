4f6c4beaf3e62f93e40863f91e55b7eb36b091d4
grHarshan <g.r.harshan999@gmail.com>
2025-05-26 01:02:23 +0530

    game 2 and 3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,12 @@ import 'game4.dart';
 import 'game5.dart';
 import 'game6.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   final List<Map<String, dynamic>> games = [
     {'title': 'Game 1', 'color': Colors.yellow[200], 'page': Game1()},
     {'title': 'Game 2', 'color': Colors.purple[300], 'page': Game2()},
@@ -15,6 +20,57 @@ class HomePage extends StatelessWidget {
     {'title': 'Game 5', 'color': Colors.lightGreenAccent[100], 'page': Game5()},
     {'title': 'Game 6', 'color': Colors.orange[200], 'page': Game6()},
   ];
+
+  late List<AnimationController> _controllers;
+  late List<Animation<Offset>> _animations;
+  List<bool> _hovering = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _controllers = List.generate(
+      games.length,
+      (index) => AnimationController(
+        duration: Duration(milliseconds: 600),
+        vsync: this,
+      ),
+    );
+
+    _animations = _controllers
+        .map((controller) => Tween<Offset>(
+              begin: Offset(0, 0.6),
+              end: Offset.zero,
+            ).animate(CurvedAnimation(
+              parent: controller,
+              curve: Curves.elasticOut, // More playful
+            )))
+        .toList();
+
+    _hovering = List.generate(games.length, (_) => false);
+
+    for (int i = 0; i < _controllers.length; i++) {
+      Future.delayed(Duration(milliseconds: 100 * i), () {
+        _controllers[i].forward();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  Route _createRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, _, child) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,58 +81,73 @@ class HomePage extends StatelessWidget {
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/H1.png'), // Updated image path
+                image: AssetImage('assets/images/H1.png'),
                 fit: BoxFit.cover,
               ),
             ),
           ),
-          // Vertical List of Games
+          // Game Buttons
           Center(
             child: SingleChildScrollView(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: games.map((game) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (_) => game['page']),
-                        );
-                      },
-                      child: Container(
-                        width: 250,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: game['color'],
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black26,
-                              blurRadius: 4,
-                              offset: Offset(2, 3),
-                            ),
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            game['title'],
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                              fontFamily: 'ComicSans', // optional
-                              shadows: [
-                                Shadow(blurRadius: 2, color: Colors.black),
-                              ],
+                children: List.generate(games.length, (index) {
+                  final game = games[index];
+                  return SlideTransition(
+                    position: _animations[index],
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: MouseRegion(
+                        onEnter: (_) {
+                          setState(() => _hovering[index] = true);
+                        },
+                        onExit: (_) {
+                          setState(() => _hovering[index] = false);
+                        },
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context)
+                                .push(_createRoute(game['page']));
+                          },
+                          child: AnimatedScale(
+                            scale: _hovering[index] ? 1.08 : 1.0,
+                            duration: Duration(milliseconds: 200),
+                            curve: Curves.easeOut,
+                            child: AnimatedContainer(
+                              duration: Duration(milliseconds: 200),
+                              width: 260,
+                              height: 65,
+                              decoration: BoxDecoration(
+                                color: game['color'],
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: _hovering[index]
+                                        ? Colors.white.withOpacity(0.6)
+                                        : Colors.black26,
+                                    blurRadius: _hovering[index] ? 12 : 4,
+                                    offset: Offset(2, 4),
+                                  ),
+                                ],
+                              ),
+                              child: Center(
+                                child: Text(
+                                  game['title'],
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontFamily: 'ComicSans',
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                   );
-                }).toList(),
+                }),
               ),
             ),
           ),
